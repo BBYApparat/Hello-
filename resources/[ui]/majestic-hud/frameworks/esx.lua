1cd569a86b5aceaae494575d903629252af8d061
Arvanitis Alexios <alexisarvas2005@gmail.com>
2025-08-27 11:50:55 +0300

    Hud

---
+++ resources/[ui]/majestic-hud/frameworks/esx.lua
@@ -0,0 +1,60 @@
+local ESX = exports['es_extended']:getSharedObject()
+
+local Framework = {}
+Framework.__index = Framework
+
+function Framework.new()
+    local self = setmetatable({}, Framework)
+    self.playerData = {}
+    self.stats = {
+        hunger = 0,
+        thirst = 0,
+        stress = 0
+    }
+    
+    -- Listen for ESX status updates
+    AddEventHandler("esx_status:onTick", function(data)
+        for i = 1, #data do
+            if data[i].name == "hunger" then
+                self.stats.hunger = math.floor(data[i].percent)
+            elseif data[i].name == "thirst" then
+                self.stats.thirst = math.floor(data[i].percent)
+            elseif data[i].name == "stress" then
+                self.stats.stress = math.floor(data[i].percent)
+            end
+        end
+    end)
+    
+    -- ESX Player Loading
+    RegisterNetEvent('esx:playerLoaded', function(xPlayer)
+        self.playerData = xPlayer
+    end)
+    
+    RegisterNetEvent('esx:onPlayerLogout', function()
+        self.playerData = {}
+    end)
+    
+    return self
+end
+
+function Framework:getPlayerData()
+    return self.playerData
+end
+
+function Framework:getStats()
+    return self.stats
+end
+
+function Framework:isPlayerLoaded()
+    if Config.framework == 'esx' then
+        return ESX.IsPlayerLoaded()
+    end
+    return self.playerData and next(self.playerData) ~= nil
+end
+
+function Framework:triggerStressGain(amount)
+    -- ESX stress system integration - add stress instead of remove
+    TriggerServerEvent('esx_status:add', 'stress', amount)
+end
+
+return Framework
---
+++ resources/[ui]/majestic-hud/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game "gta5"
+
+description 'MajesticDev + Skeleton Network'
+version '1.0.0'
+lua54 'yes'
+
+ui_page 'build/index.html'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua',
+}
+
+client_script 'client.lua'
+
+files {
+    'locales/*.json',
+    'build/**',
+    'frameworks/*.lua',
+}
+
+dependency '/assetpacks'
